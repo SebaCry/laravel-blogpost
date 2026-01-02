8b2abedd5d704e7a367ddcd9b07cc07a8bc66374
Sebastián Pérez <Jspc513@gmail.com>
2026-01-02 15:06:51 -0500

    add new dinamic functions in views

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,20 +1,35 @@
 <x-forum.layouts.app>
-<ul>
-    <li>
-        {{ $blog->title }}
-    </li>
-    <li>
-        {{ $blog->content }}
-    </li>
-    {{-- <li>
-        <h3>Respuestas:</h3>
-        @foreach($question->answers as $answer)
-            <div>
-                <p>{{ $answer->content }}</p>
-                <small>Por: {{ $answer->user_id }}</small>
+<div class="flex items-center gap-2 w-full my-8">
+    <div>&hearts;</div>
+    <div class="w-full">
+        <h2 class="text-2xl font-bold md:text-3xl">
+            {{ $blog->title }}
+        </h2>
+
+        <div class="flex justify-between">
+            <p class="text-xs text-gray-500">
+                <span class="font-semibold">
+                {{ $blog->user->name }}
+                </span> |
+                {{ $blog->category->name }} |
+                {{ $blog->created_at->diffForHumans() }}
+            </p>
+
+            <div class="flex items-center gap-2">
+                <a href="#" class="text-xs font-semibold hover:underline">
+                    Edit
+                </a>
+
+                <form action="#" onsubmit="return confirm('¿Estás seguro de eliminar esta pregunta?');">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="rounded-md bg-red-600 hover:bg-red-500 px-2 py-1 text-xs font-semibold text-white cursor-pointer">
+                        Eliminar
+                    </button>
+                </form>
             </div>
-        @endforeach
-    </li> --}}
-</ul>
+        </div>
+    </div>
+</div>
 </x-forum.layouts.app>
 
